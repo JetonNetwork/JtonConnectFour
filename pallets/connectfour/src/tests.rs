4c16025abcb200b267cad923e909c6de7cb0016c
darkfriend77 <darkfriend@swissonline.ch>
2021-06-22 23:04:11 +0200

    added force turn test

--- pallets/connectfour/src/tests.rs
+++ pallets/connectfour/src/tests.rs
@@ -168,3 +168,49 @@ fn test_game_events() {
 		assert_eq!(77, ConnectFour::something().unwrap());
 	});
 }
+
+#[test]
+fn test_force_turn() {
+	new_test_ext().execute_with(|| {
+
+		let mut current_block:u64 = 100;
+
+		// start from block 100
+		run_to_block(current_block);
+
+		// Test game creation between to different players
+		assert_ok!(ConnectFour::new_game(Origin::signed(PLAYER_1 as u64), PLAYER_2 as u64));
+		let board_id = ConnectFour::player_board(PLAYER_1 as u64);
+		let board = ConnectFour::boards(board_id);
+		assert_eq!(board.last_turn, current_block);
+
+		run_next_block();
+		current_block = current_block + 1;
+
+		assert_eq!(System::block_number(), current_block);
+
+		if board.next_player == PLAYER_1 {
+			assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_1 as u64), 0));
+			let board = ConnectFour::boards(board_id);
+			assert!(board.board_state == BoardState::Running);	
+			assert!(board.next_player == PLAYER_2);
+			assert_eq!(board.last_turn, current_block);
+
+			run_next_block();
+			current_block = current_block + 1;
+		}
+
+		assert_ok!(ConnectFour::play_turn(Origin::signed(PLAYER_2 as u64), 1));
+		let board = ConnectFour::boards(board_id);
+		assert_eq!(board.last_turn, current_block);
+		assert!(board.board_state == BoardState::Running);
+		assert!(board.next_player == PLAYER_1);
+
+		run_to_block(current_block + 10);
+		current_block = current_block + 10;
+
+		let board = ConnectFour::boards(board_id);
+		assert_eq!(board.last_turn, current_block);
+		assert!(board.board_state == BoardState::Finished(board.blue));
+	});
+}
